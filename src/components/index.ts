dad1489894c4e0afd83338b0cce2cd00339175d4
monel14 <62266181+monel14@users.noreply.github.com>
2025-08-15 02:30:24 +0100

    feat: Update dependencies and import maps
    Updates project dependencies to React 19 RC and Video.js v8.x. Also, adjusts the import map in `index.html` to align with the new dependency versions and resolves potential path issues in Vite configuration. The `src/components` and `src/components/ui` export files are cleared as they are not currently used.

--- src/components/index.ts
+++
@@ -1,6 +0,0 @@
-export { CountryList } from './CountryList';
-export { ChannelGrid } from './ChannelGrid';
-export { VideoPlayer } from './VideoPlayer';
-export { PlayerOverlay } from './PlayerOverlay';
-export * from './ui';
-export * from './icons';
--- src/components/ui/index.ts
+++
@@ -1,5 +0,0 @@
-export { Loader } from './Loader';
-export { SearchInput } from './SearchInput';
-export { Header } from './Header';
-export { StatusIndicator } from './StatusIndicator';
-export { ImageWithFallback } from './ImageWithFallback';
